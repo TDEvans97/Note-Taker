dfb7341502f0d4949846cf1fa8819a93e83beac6
TDEvans97 <tylerdevansmusic@gmail.com>
2022-07-25 07:55:49 -0400

    Updated code in apiRoutes.js to properly utilize helper functions and return alerts.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,13 @@
 const api = require("express").Router();
 
+// Import our helper functions (read, write, getNotes, addNote, removeNote)
 const store = require("../helpers/store");
 
 // Get all the notes on page load
 api.get("/notes", (req, res) => {
   console.log(`${req.method} for /api/notes`);
   store.getNotes().then((notes) => {
-      res.json(notes)
+    res.json(notes)
   })
 });
 
@@ -15,18 +16,16 @@ api.post("/notes", (req, res) => {
   console.log(`${req.method} for /api/notes`);
   store.addNote(req.body);
   res.json(req.body);
-  return;
+  //Signal the user that their post request has been calculated.
+  return alert("New note has been added!");
 });
 
 // Delete a note from the page
 api.delete("/notes/:id", (req, res) => {
   console.log(`${req.method} for /api/notes`);
-  if (!res) {
-      console.log("err");
-  } else {
-      store.removeNote(req.params.id);
-      res.status("Note deleted");
-  }
+  // removeNote will return an err if no id match exists
+  store.removeNote(req.params.id);
+  res.alert("The note has been deleted!");
 });
 
 module.exports = api;
